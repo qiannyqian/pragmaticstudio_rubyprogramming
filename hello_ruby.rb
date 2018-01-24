73423e452f80613da2993603d0c5fc4f1ca1d705
Hui Qian <liang.huiqian@gmail.com>
2018-01-24 15:07:38 +0800

    New updated progress on course

---
+++ hello_ruby.rb
@@ -0,0 +1,20 @@
+comment =  "Ruby is fun!"
+puts comment.upcase
+
+#this is called a block
+3.times do
+  puts comment.upcase
+end
+
+puts Time.new
+
+
+greeting = "Welcome!"
+
+puts greeting
+
+3.times do
+  puts greeting.upcase
+end
+
+puts Time.now
---
+++ studio_game/studio_game.rb
@@ -0,0 +1,9 @@
+name1 = "larry"
+health1 = 60
+
+#puts "#{name1}'s health is #{health1/9}"
+
+name2 = "curly"
+name3 = "moe"
+
+puts "Players:\n\t#{name1}\n\t#{name2}\n\t#{name3}"
